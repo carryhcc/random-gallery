0125fe498b2e1d7ca39efd31c8db63b5e187c504
胡长城 <hcc_sf@jztey.com>
2025-06-25 17:20:58 +0800

    update

--- src/main/java/com/example/randomGallery/controller/PicController.java
+++ src/main/java/com/example/randomGallery/controller/PicController.java
@@ -7,6 +7,7 @@ import com.example.randomGallery.server.PicServiceApi;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -44,22 +45,18 @@ public class PicController {
      * @throws SQLException
      */
     @GetMapping("/pic/list")
-    public Map<String, String> picList() throws SQLException {
+    @ResponseBody
+    public Map<String, String> picList() {
         cacheService.resetTimer();
         Integer randomGroupId = cacheService.getRandomGroupId();
         PicGroupVO randomGroupPicList = picServiceApi.getRandomGroupPicList(randomGroupId);
         Map<String, String> map = new HashMap<>();
-        map.put(randomGroupPicList.getGroupName(), "[" + randomGroupPicList.getUrlList() + "]");
+        map.put(randomGroupPicList.getGroupName(), randomGroupPicList.getUrlList().toString());
         return map;
     }
 
     @GetMapping("/showPicList")
     public ModelAndView showPicListPage() {
         return new ModelAndView("picList");
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        return "/testHtml";
     }
 }
