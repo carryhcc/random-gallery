cf725910c0539b7e4f7cb1dbb81b5941a388aac4
cchu <hccsoul@gmail.com>
2025-06-29 17:31:51 +0800

    update do

--- src/main/java/com/example/randomGallery/entity/VO/PicGroupVO.java
+++ src/main/java/com/example/randomGallery/entity/VO/PicGroupVO.java
@@ -11,4 +11,20 @@ public class PicGroupVO implements Serializable {
     public String groupName;
 
     public List<String> urlList;
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public List<String> getUrlList() {
+        return urlList;
+    }
+
+    public void setUrlList(List<String> urlList) {
+        this.urlList = urlList;
+    }
 }
