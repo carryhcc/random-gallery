abbe8eb112b09304660de4e7329db10966c4bd31
cchu <hccsoul@gmail.com>
2025-06-29 17:27:04 +0800

    update ftl

--- src/main/java/com/example/randomGallery/entity/DO/PicInfoDO.java
+++ src/main/java/com/example/randomGallery/entity/DO/PicInfoDO.java
@@ -11,4 +11,44 @@ public class PicInfoDO implements Serializable {
     private String picName;
     private Integer groupId;
     private Integer isDelete;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        this.picUrl = picUrl;
+    }
+
+    public String getPicName() {
+        return picName;
+    }
+
+    public void setPicName(String picName) {
+        this.picName = picName;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
 }
