db5f04a501e2a6184ee6677446d26fb6615d0698
胡长城 <hcc_sf@jztey.com>
2026-01-08 09:41:01 +0800

    upup4

--- src/main/java/com/example/randomGallery/service/mapper/AuthorMapper.java
+++ src/main/java/com/example/randomGallery/service/mapper/AuthorMapper.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.randomGallery.entity.DO.AuthorDO;
 import com.example.randomGallery.entity.VO.AuthorVO;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -14,10 +13,5 @@ import java.util.List;
 @Mapper
 public interface AuthorMapper extends BaseMapper<AuthorDO> {
 
-    @Select("SELECT a.author_id as authorId, a.author_nickname as authorNickname, a.author_url as authorUrl, COUNT(aw.work_id) as workCount "
-            +
-            "FROM author a " +
-            "LEFT JOIN author_work aw ON a.author_id = aw.author_id " +
-            "GROUP BY a.author_id, a.author_nickname, a.author_url")
     List<AuthorVO> selectAuthorsWithWorkCount();
 }
--- src/main/java/com/example/randomGallery/service/mapper/TagMapper.java
+++ src/main/java/com/example/randomGallery/service/mapper/TagMapper.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.randomGallery.entity.DO.TagDO;
 import com.example.randomGallery.entity.VO.TagVO;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -14,9 +13,5 @@ import java.util.List;
 @Mapper
 public interface TagMapper extends BaseMapper<TagDO> {
 
-    @Select("SELECT t.id, t.tag_name as tagName, COUNT(tw.work_id) as workCount " +
-            "FROM tags t " +
-            "LEFT JOIN tag_work tw ON t.id = tw.tag_id " +
-            "GROUP BY t.id, t.tag_name")
     List<TagVO> selectTagsWithWorkCount();
 }
